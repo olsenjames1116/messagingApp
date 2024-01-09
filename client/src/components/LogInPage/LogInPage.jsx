f087a1ab78d855738aec4d194b9b01e6201dd10c
James Olsen <olsenjames1116@gmail.com>
2024-01-09 18:30:48 -0500

    Create skeleton for log in page

--- client/src/components/LogInPage/LogInPage.jsx
+++ client/src/components/LogInPage/LogInPage.jsx
@@ -1,5 +1,28 @@
+import { userImage } from '../../assets/images';
+
 function LogInPage() {
-	return <div>LogInPage</div>;
+	const handleSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<main>
+			<div className="content">
+				<div className="title">App Title</div>
+				<div className="input">
+					<div className="banner">
+						<img src={userImage} />
+						<span>Welcome!</span>
+					</div>
+					<form method="POST" action="" onSubmit={handleSubmit}>
+						<input type="text" id="username" name="username" />
+						<input type="password" id="password" name="password" />
+						<button>Log In</button>
+					</form>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default LogInPage;
