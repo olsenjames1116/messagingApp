6652c426e4f058a56b8f3d8a1db0ab01b0547699
James Olsen <olsenjames1116@gmail.com>
2024-01-11 16:46:08 -0500

    Retrieve user info from storage if page is reloaded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
 import Router from './Router';
+import { useDispatch } from 'react-redux';
+import { addUser } from './redux/state/userSlice';
 
 function App() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const username = localStorage.getItem('username');
+		const bio = localStorage.getItem('bio');
+		const profilePic = localStorage.getItem('profilePic');
+
+		const user = { username: username, bio: bio, profilePic: profilePic };
+
+		username && dispatch(addUser(user));
+	}, []);
+
 	return (
 		<>
 			<Router />
