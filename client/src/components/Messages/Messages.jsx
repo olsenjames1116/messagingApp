7f32e4912bf648cd03310396f891feff375fce21
James Olsen <olsenjames1116@gmail.com>
2024-03-19 21:17:44 -0400

    Format messages so they can overflow

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import styles from './Messages.module.css';
 
 // Represents the messages between users displayed in the chat box.
 function Messages() {
@@ -8,7 +9,7 @@ function Messages() {
 	const user = useSelector((state) => state.user);
 
 	return (
-		<ul className="messages">
+		<ul className={styles.messages}>
 			{
 				// Check if there is an array stored in state. If so, display the messages.
 				Array.isArray(messagesBetweenUsers) &&
@@ -16,14 +17,20 @@ function Messages() {
 						if (message.from === user._id) {
 							// This message is from the user.
 							return (
-								<li key={message._id} className="fromUser">
+								<li
+									key={message._id}
+									className={`${styles.fromUser} ${styles.message}`}
+								>
 									{message.message}
 								</li>
 							);
 						} else {
 							// This message has been received by the user.
 							return (
-								<li key={message._id} className="toUser">
+								<li
+									key={message._id}
+									className={`${styles.toUser} ${styles.message}`}
+								>
 									{message.message}
 								</li>
 							);
