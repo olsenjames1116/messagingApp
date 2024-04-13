df12eb2e6faa29266b7072f85a9532565d4cbc15
James Olsen <olsenjames1116@gmail.com>
2024-04-13 15:32:53 -0400

    Add copyrights

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import './Footer.css';
 function Footer() {
 	return (
 		<footer>
+			© 2024 James Olsen. All Rights Reserved.
 			<p>
 				Built and designed by{' '}
 				<a href="https://github.com/olsenjames1116">olsenjames1116</a>
